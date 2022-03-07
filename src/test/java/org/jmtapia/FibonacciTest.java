1cf62685b3a2a1b81b3f7186afd77e4d1a7c7282
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-03-07 21:50:35 +0100

    create a FibonacciTest.java and Test for base cases

--- src/test/java/org/jmtapia/FibonacciTest.java
+++ src/test/java/org/jmtapia/FibonacciTest.java
@@ -1,4 +1,37 @@
 package org.jmtapia;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test cases:
+ * fibonacci 0 -> 0
+ * fibonacci 1 -> 1
+ * fibonacci 2 -> 1
+ * fibonacci 3 -> 2
+ * fibonacci 4 -> 3
+ */
+
+
 public class FibonacciTest {
+
+
+    @Test
+    public void shouldComputeReturnZeroIfTheNumberIsZero(){
+        Fibonacci fib = new Fibonacci();
+        int expectedValue = 0;
+        int obtainedValue = fib.compute(0);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+
+    @Test
+    public void shouldComputeReturnOneIfTheNumberIsOne(){
+        Fibonacci fib = new Fibonacci();
+        int expectedValue = 1;
+        int obtainedValue = fib.compute(1);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
 }
