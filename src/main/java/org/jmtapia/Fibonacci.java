c3fe4135f79c32593e4876049b7a00bb9c8f5cac
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-03-07 21:40:56 +0100

    implematation class Fibonacci.java

--- src/main/java/org/jmtapia/Fibonacci.java
+++ src/main/java/org/jmtapia/Fibonacci.java
@@ -1,4 +1,21 @@
 package org.jmtapia;
 
+/**
+ * Class providing a method to compute fibonacci sucesion
+ *
+ * @author Jose M. Tapia
+ */
+
 public class Fibonacci {
+    public int compute (int value){
+        int result = 0;
+        if (value < 0){
+            throw new RuntimeException("The values cannot be a negative number: " + value);
+        } else if (value == 1){
+            result = 1;
+        } else if (value > 1){
+            result = compute(value-2) + compute(value-1);
+        }
+        return result;
+    }
 }
