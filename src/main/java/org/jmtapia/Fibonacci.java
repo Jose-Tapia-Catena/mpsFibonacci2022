11e758c139085f61833878a88a6bc5745d7b7afc
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-03-09 16:48:10 +0100

    Fibonacci iterativo

--- src/main/java/org/jmtapia/Fibonacci.java
+++ src/main/java/org/jmtapia/Fibonacci.java
@@ -7,16 +7,19 @@ package org.jmtapia;
  */
 
 public class Fibonacci {
-    public int compute (int value){
-        int result = 0;
+    public int compute (int value) {
+        int a = 0;
+        int b = 1;
+
         //si valor es 0, es otro caso base que sabemos que el resultado = 0
-        if (value < 0){
+        if (value < 0) {
             throw new RuntimeException("The values cannot be a negative number: " + value);
-        } else if (value == 1){ //caso base
-            result = 1;
-        } else if (value > 1){
-            result = compute(value-2) + compute(value-1);
+        } else {
+            for (int i = 0; i <value; i++) {
+                b = b + a;
+                a = b - a;
+            }
         }
-        return result;
+        return a;
     }
 }
